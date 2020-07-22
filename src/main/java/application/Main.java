ee429c9ab303285477224c44415df4877c8983b5
alfredoisrael-lopez <alfredoisrael-lopez@users.noreply.github.com>
2020-07-22 09:53:41 -0500

    changing the send message method

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -30,7 +30,7 @@ public class Main {
 		try {
 			if (log.isDebugEnabled())
         		log.debug("send hello world");
-			jmsTemplate.convertAndSend("DEV.QUEUE.1", "Message from Cloud Pak 4 Applications to demo AMX");
+			jmsTemplate.convertAndSend("DEV.QUEUE.1", "Message to QUEUE1");
 			return "OK";
 		} catch(JmsException ex) {
 			ex.printStackTrace();
